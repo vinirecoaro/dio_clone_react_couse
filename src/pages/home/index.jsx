86950ac58b38f1da7279f275c484459e2e84c9fb
Vinicius Recoaro <vrecoaro@hotmail.com>
2026-06-27 11:15:53 -0300

    Home screen - completed initial layout

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,34 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
+import { Container, TextContent, Title, TitleHighLight } from './styles'
+import bannerImage from '../../assets/banner.png'
 
 const Home = () => {
     return (
         <>
             <Header></Header>
+            <Container>
+                <div>
+                    <Title>
+                        <TitleHighLight>
+                            Implemente
+                            <br />
+                            o seu futuro global agora !
+                        </TitleHighLight>
+                        <TextContent>
+                            Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo
+                            desafio profissional, evoluindo em comunidade com os melhores experts. 
+                        </TextContent>
+                        <Button title={"Começar agora"} variant='secondary' onClick={() => null}></Button>
+                    </Title>
+                </div>
+                <div>
+                    <img src={bannerImage} alt="Imagem principal" />
+                </div>
+            </Container>
         </>
+        
     )
 }
 
